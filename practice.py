7c467b4ae156d2f9227bfc7f894f0a51c578641d
ggwnsghgg <43259783+ggwnsghgg@users.noreply.github.com>
2020-09-28 22:18:53 +0900

    practice python

--- practice.py
+++ practice.py
@@ -1273,4 +1273,261 @@
 
     # 문제
 
+    # 총 3대의 매물이 있습니다.
+    # 강남 아파트 매매 10억 2010년
+    # 마포 오피스텔 전세 5억 2007년
+    # 송파 빌라 월세 500/50 2000년
+
+# class House:
+#         # 매물 초기화
+#         def __init__(self, location, house_type, deal_type, price, completion_year):
+#             self.location = location
+#             self.house_type = house_type
+#             self.deal_type = deal_type
+#             self.price = price
+#             self.completion_year = completion_year    
+        
+#         # 매물 정보 표시
+#         def show_detail(self):
+#             print(self.location, self.house_type, self.deal_type \
+#             , self.price, self.completion_year)
+        
+
+# houses = []
+# house1 = House("강남", "아파트", "매매", "10억", "2010년")
+# house2 = House("마포", "오피스텔", "전세", "5억", "2007년")
+# house3 = House("송파", "빌라", "월세", "500/50", "2010년")
+
+
+# houses.append(house1)
+# houses.append(house2)
+# houses.append(house3)
+
+# print("총 {0}대의 매물이 있습니다.".format(len((houses))))
+# for house in houses:
+#     house.show_detail()
+
+
+
+
+# ------------------------------------------------------
+
+
+
+
+    # 예외처리
+
+# try:
+#     print("나누기 전용 계산기 입니다.")
+#     nums = []
+#     nums.append(int(input("첫 번쨰 숫자를 입력하세요 : ")))
+#     nums.append(int(input("두 번쨰 숫자를 입력하세요 : ")))
+#     nums.append(int(nums[0] / nums[1]))
+#     print("{0} / {1} = {2} ".format(nums[0], nums[1], nums[2]))
+# except ValueError:
+#     print("에러! 잘못된 값을 입력하였습니다.")
+# except ZeroDivisionError as err:
+#     print(err)
+# except Exception as err:
+#     print("알 수 없는 에러가 발생하였습니다.")
+#     print(err)
+
+
+
+
+# ------------------------------------------------------
+
+
+
+
+    # 에러 발생시키기
+
+# try:
+#     print("한 자리 숫자 나누기 전용 계산기 입니다.")
+#     num1 = int(input("첫 번째 숫자를 입력하세요 : "))
+#     num2 = int(input("첫 번째 숫자를 입력하세요 : "))
+#     if num1 >= 10 or num2 >= 10:
+#         raise ValueError
+#     print("{0} / {1} = {2} ".format(num1, num2, int(num1/num2)))
+# except ValueError:
+#     print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
+    
+
+
+
+# # ------------------------------------------------------
+
+
+
+
+#     # 사용자 정의 예외처리
+# class BigNumberError(Exception):
+#     def __init__(self, msg):
+#         self.msg = msg
+    
+#     def __str__(self):
+#         return self.msg
+
+# try:
+#     print("한 자리 숫자 나누기 전용 계산기 입니다.")
+#     num1 = int(input("첫 번째 숫자를 입력하세요 : "))
+#     num2 = int(input("첫 번째 숫자를 입력하세요 : "))
+#     if num1 >= 10 or num2 >= 10:
+#         raise BigNumberError("입력값 : {0}, {1}".format(num1, num2))
+#     print("{0} / {1} = {2} ".format(num1, num2, int(num1/num2)))
+# except ValueError:
+#     print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
+# except BigNumberError as err:
+#     print("에러가 발생하였습니다. 한 자리 숫자만 입력하세요.")
+#     print(err)
+
+
+
+
+# ------------------------------------------------------
+
+
+
+
+    # finally
+
+# class BigNumberError(Exception):
+#     def __init__(self, msg):
+#         self.msg = msg
+    
+#     def __str__(self):
+#         return self.msg
+
+# try:
+#     print("한 자리 숫자 나누기 전용 계산기 입니다.")
+#     num1 = int(input("첫 번째 숫자를 입력하세요 : "))
+#     num2 = int(input("첫 번째 숫자를 입력하세요 : "))
+#     if num1 >= 10 or num2 >= 10:
+#         raise BigNumberError("입력값 : {0}, {1}".format(num1, num2))
+#     print("{0} / {1} = {2} ".format(num1, num2, int(num1/num2)))
+# except ValueError:
+#     print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
+# except BigNumberError as err:
+#     print("에러가 발생하였습니다. 한 자리 숫자만 입력하세요.")
+#     print(err)
+# finally:
+#     print("계산기를 이용해 주셔서 감사합니다.")
+
+
+
+
+# ------------------------------------------------------
+
+
+
+
+    # 문제
+
+# 동네 항상 대기 손님이 있는 맛있는 치킨집이 있습니다.
+# 대기 손님의 치킨 요리 시간을 줄이고자 자동 주문 시스템을 제작하였습니다.
+# 시스템 코드를 확인하고 적절한 예외처리 구문을 넣으시오.
+
+# 조건1 : 1보다 작거나 숫자가 아닌 입력값이 들어올 떄는 ValueError 로 처리
+#         출력 메세지 : "잘못된 값을 입력하였습니다."
+# 조건2 : 대기 손님이 주문할 수 있는 총 치킨량은 10마리로 한정
+#         치킨 소진 시 사용자 정의 에러[SoldOutError]를 발생시키고 프로그램 종료
+#         출력 메세지 : "재고가 소진되어 더 이상 주문을 받지 않습니다."
+
+
+# class SoldOutError(Exception):
+#     pass
+
+# chicken = 10
+# waiting = 1
+# while(True):
+#     try:
+#         print("[남은치킨 {0}]".format(chicken))
+#         order = int(input("치킨 몇 마리 주문하시겠습니까 ? "))
+#         if order > chicken:
+#             print("재료가 부족합니다")
+#         elif order <= 0:
+#             raise ValueError
+#         else:
+#             print("[대기번호 {0}] {1} 마리 주문이 완료되었습니다" \
+#                 .format(waiting, order))
+#             waiting += 1
+#             chicken -= order
+#         if chicken == 0:
+#             raise SoldOutError
+
+#     except ValueError:
+#         print("잘못된 값을 입력하였습니다.")
+#     except SoldOutError:
+#         print("재고가 소진되어 더 이상 주문을 받지 않습니다.")
+#         break        
+
+
+
+
+# ------------------------------------------------------
+
+
+
+
+    # 모듈
+
+# import teather_module
+# teather_module.price(3) # 3명이 영화 보러 갔을 때 가격
+# teather_module.price_moring(4)  # 4명이 조조 할인 영화 보러 갔을 떄 가격
+# teather_module.price_solder(5)  # 5명이 군인이 영화 보러 갔을 때 가격 
+
+
+# import teather_module as mv
+# mv.price(3)
+# mv.price_moring(4)
+# mv.price_solder(5)
+
+# from teather_module import *
+# price(3)
+# price_moring(4)
+# price_solder(5)
+
+# from teather_module import price, price_moring
+# price(5)
+# price_moring(6)
+
+# from teather_module import price_solder as price
+# price(5)
+
+
+# import travel.thailand
+# import travel.thailand.ThaillandPackage
+# trip_to = travel.thailand.ThaillandPackage()
+# trip_to.detail()
+
+# from travel.thailand import ThaillandPackage
+# trip_to = ThaillandPackage()
+# trip_to.detail()
+
+# from travel import vietnam
+# trip_to = vietnam.VietnamPackage()
+# trip_to.detail()
+
+# from travel import *
+# # # trip_to = vietnam.VietnamPackage()
+# # trip_to = thailand.ThaillandPackage()
+# # trip_to.detail()
+
+# import inspect
+# import random
+# print(inspect.getfile(random))
+# print(inspect.getfile(thailand))
+
+
+
+
+# ------------------------------------------------------
+
+
+
+
+    # 패키지 설치
+
+
+
 
